672af6f5bc307cce174e698c6a18133ac3621138
Eric Tian <erictianc@outlook.com>
2024-09-06 11:54:16 +0800

    自动获取 semesterId

--- code/provider.js
+++ code/provider.js
@@ -3,11 +3,16 @@ async function scheduleHtmlProvider() {
   try {
     var res = await fetch('https://jw.ustc.edu.cn/for-std/course-table',{
       method: 'GET',
-    }) // 获取 dataId
+    }) 
 
     path_parts = res.url.split('/')
-    const dataId = parseInt(path_parts[path_parts.length-1])
-    res = await fetch(`https://jw.ustc.edu.cn/for-std/course-table/semester/362/print-data/${dataId}?weekIndex=`)
+    const dataId = parseInt(path_parts[path_parts.length-1]) // 获取 dataId
+
+    res = await fetch('https://jw.ustc.edu.cn/for-std/course-table').then(data => data.text())
+    var doc = new DOMParser().parseFromString(res, 'text/xml')
+    const semesterId = doc.querySelectorAll('option[selected="selected"]')[0].value
+
+    res = await fetch(`https://jw.ustc.edu.cn/for-std/course-table/semester/${semesterId}/print-data/${dataId}?weekIndex=`)
     return await res.text()
   } catch (error) {
     console.error(error)
